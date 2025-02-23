0ba1550b0ba8aafb62bbdc7925047a05d3f850c7
Edgaras <cedgaras112@gmail.com>
2025-02-23 16:07:13 +0200

    major update filmu saraso vaizdavimo funkcionalume//prideta atributu atnaujinimo feature

--- app.py
+++ app.py
@@ -48,16 +48,19 @@ class Organizatorius:
             return False
 
 
-    def istrinti_filma(filmas):
-            try:
-                Organizatorius.filmu_biblioteka.pop(filmas)
-                with open("filmu_biblioteka_pickle.pickle","wb") as failas: #pickle.dump(turinys,failas) //PRIDETA dump
-                    pickle.dump(Organizatorius.filmu_biblioteka, failas)
+    def istrinti_filma(raktazodis):
+        rastas = False
+        for filmas in Organizatorius.filmu_biblioteka.copy():
+            if raktazodis.lower() in filmas.pavadinimas.lower() and raktazodis != '':
+                Organizatorius.filmu_biblioteka.remove(filmas)
+                rastas = True
+        if rastas == True:
+            with open("filmu_biblioteka_pickle.pickle","wb") as failas:
+                pickle.dump(Organizatorius.filmu_biblioteka,failas)
                 return True
-            except:
-                raise Exception ('Ivedete netinkama filma.')
-                    
-
+        elif rastas == False:
+            return False
+            
 class Ziurovas:
     def __init__(self,amzius):
           self.vartotojo_vardas = amzius
@@ -121,10 +124,44 @@ while True:
             zodynas_is_failo = Organizatorius.perziureti_filmus()
             for filmas in zodynas_is_failo:
                 print(filmas)
-            trinti_naujinti_grizti = input('Tolimesnis veiksmas:\n 1. Pasalinti filma pagal pavadinima \n 2. Atnaujinti filmu duomenis\n3. Grizti')
+            trinti_naujinti_grizti = input('Tolimesnis veiksmas:\n1. Pasalinti filma pagal pavadinima \n2. Atnaujinti filmu duomenis\n3. Grizti\nJusu pasirinkimas: ')
             if trinti_naujinti_grizti == '1':
-                trinamo_filmo_pavadinimas = input('Iveskite norimo istrinti filmo pavadinima: ')
-                
+                while True:
+                    trinamo_filmo_pavadinimas = input('Iveskite norimo istrinti filmo pavadinima: ')
+                    if Organizatorius.istrinti_filma(trinamo_filmo_pavadinimas) == True:
+                        print(f"Filmas '{trinamo_filmo_pavadinimas}' sekmingai istrintas.")
+                        break
+                    elif Organizatorius.istrinti_filma(trinamo_filmo_pavadinimas) == False:
+                        print("Toks filmo pavadinimas neegzistuoja. Bandykite dar karta arba spauskite ENTER, kad griztumete i meniu.")
+                        if trinamo_filmo_pavadinimas == '':
+                            break
+            elif trinti_naujinti_grizti == '2':
+                atnaujinamas_filmas = input('Iveskite norimo atnaujinti filmo pavadinima: ')
+                rastas_filmas = Organizatorius.ieskoti_filmo(atnaujinamas_filmas)
+                if not rastas_filmas:
+                    print('Tokio filmo nera.')
+                else:
+                    filmas = rastas_filmas[0] #naudojam su indeksu nes paieskos metodas grazina sarasa.
+                    print(filmas)
+                    print("1. Pavadinimas\n2. Trukme\n3. Zanras\n4. Rezisierius\n5. Isleidimo metai\n6. Amziaus reitingas")
+                    keiciamas_parametras = input('Keiciamas parametras (1-6): ')
+                    if keiciamas_parametras == '1':
+                        filmas.pavadinimas = input('Iveskite nauja pavadinima: ')
+                    elif keiciamas_parametras == '2':
+                        filmas.trukme = input('Iveskite nauja trukme: ')
+                    elif keiciamas_parametras == '3':
+                        filmas.zanras = input('Iveskite nauja zanra: ')
+                    elif keiciamas_parametras == '4':
+                        filmas.rezisierius = input('Iveskite nauja rezisieriu: ')
+                    elif keiciamas_parametras == '5':
+                        filmas.isleidimo_m = input('Iveskite naujus isleidimo metus: ')
+                    elif keiciamas_parametras == '6':
+                        filmas.amziaus_reitingas = input('Iveskite nauja amziaus reitinga: ')
+                    with open("filmu_biblioteka_pickle.pickle", "wb") as failas:
+                        pickle.dump(Organizatorius.filmu_biblioteka, failas)
+                    print("Filmo duomenys atnaujinti.")
+
+#=================================================================================================================================
 
         elif veiksmas == '3':     # Filmu ieskojimas
             while True:
@@ -141,7 +178,7 @@ while True:
                 for filmas in surasti_filmai:
                     print(filmas)
                 print('')
-
+#=================================================================================================================================
         elif veiksmas == '4':     # Filmu istrynimas
             i = 0
             print('Kuri filma noretumete istrinti? \n')
@@ -150,6 +187,7 @@ while True:
                 i = i + 1
                 if i == len(Organizatorius.filmu_biblioteka): # cia sustoja ciklas pasiekes bibliotekos pabaiga ir tada klausia kelinta numeri triname
                     Organizatorius.istrinti_filma(int(input()))
+#=================================================================================================================================
         elif veiksmas == '5':     # Exit
             print('Viso gero.')
             break
